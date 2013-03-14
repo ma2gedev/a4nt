b8280881cbb0481f55cf6d08c4436c1dc8e2ef18
ma2ge <takayuki.1229@gmail.com>
2013-03-15 01:04:21 +0900

    Added newer method thats findout displayable announcements order by position.

--- app/models/a4nt/announcement.rb
+++ app/models/a4nt/announcement.rb
@@ -1,5 +1,7 @@
 module A4nt
   class Announcement < ActiveRecord::Base
     attr_accessible :body, :end_time, :position, :start_time, :title
+    scope :active, -> { where(["start_time <= ? AND end_time >= ?", Time.now, Time.now]) }
+    scope :newer, -> { active.order('position ASC') }
   end
 end
