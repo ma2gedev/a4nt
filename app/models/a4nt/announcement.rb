4b234c2bc9706a7829fe0b04b5f8ba21a2f16ca1
ma2ge <takayuki.1229@gmail.com>
2013-03-18 19:34:47 +0900

    Added members and non_members methods.

--- app/models/a4nt/announcement.rb
+++ app/models/a4nt/announcement.rb
@@ -1,12 +1,23 @@
 module A4nt
   class Announcement < ActiveRecord::Base
     attr_accessible :body, :end_time, :position, :start_time, :title, :target
-    TARGETS = %w(all nonmember member)
+
+    module Target
+      ALL = 'all'
+      NON_MEMBER = 'nonmember'
+      MEMBER = 'member'
+
+      def self.all
+        self.constants.map { |name| self.const_get(name) }
+      end
+    end
 
     validates :title, :body, :start_time, :end_time, :position, :target, presence: true
-    validates :target, inclusion: { in: TARGETS }
+    validates :target, inclusion: { in: Target.all }
 
     scope :active, -> { where(["start_time <= ? AND end_time >= ?", Time.now, Time.now]) }
     scope :newer, -> { active.order('position ASC') }
+    scope :non_members, -> { newer.where(target: [ Target::ALL, Target::NON_MEMBER ]) }
+    scope :members, -> { newer.where(target: [ Target::ALL, Target::MEMBER ]) }
   end
 end
