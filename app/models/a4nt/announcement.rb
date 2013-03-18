203f2034740c94ba26e5d373f2a0d1609d377508
ma2ge <takayuki.1229@gmail.com>
2013-03-18 17:36:50 +0900

    Added target column and constraints.

--- app/models/a4nt/announcement.rb
+++ app/models/a4nt/announcement.rb
@@ -1,6 +1,11 @@
 module A4nt
   class Announcement < ActiveRecord::Base
-    attr_accessible :body, :end_time, :position, :start_time, :title
+    attr_accessible :body, :end_time, :position, :start_time, :title, :target
+    TARGETS = %w(all nonmember member)
+
+    validates :title, :body, :start_time, :end_time, :position, :target, presence: true
+    validates :target, inclusion: { in: TARGETS }
+
     scope :active, -> { where(["start_time <= ? AND end_time >= ?", Time.now, Time.now]) }
     scope :newer, -> { active.order('position ASC') }
   end
--- db/migrate/20130314151144_create_a4nt_announcements.rb
+++ db/migrate/20130314151144_create_a4nt_announcements.rb
@@ -1,11 +1,12 @@
 class CreateA4ntAnnouncements < ActiveRecord::Migration
   def change
     create_table :a4nt_announcements do |t|
-      t.string :title
-      t.string :body
-      t.datetime :start_time
-      t.datetime :end_time
-      t.integer :position
+      t.string :title,          null: false,    default: ''
+      t.string :body,           null: false,    default: ''
+      t.datetime :start_time,   null: false
+      t.datetime :end_time,     null: false
+      t.integer :position,      null: false
+      t.string :target,         null: false,    default: 'all'
 
       t.timestamps
     end
